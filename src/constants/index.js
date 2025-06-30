99f6c2a83c51d8e56060315a468eadf6e2586985
newcomer119 <mitarth.125035@stu.upes.ac.in>
2025-06-30 20:52:32 +0530

    made changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -36,7 +36,7 @@ export const clientReviews = [
     position: 'Founder of TechGear Shop',
     img: 'assets/review2.png',
     review:
-      'Mitarth’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.',
+      "Mitarth's expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He's a true professional! Fantastic work.",
   },
   {
     id: 3,
@@ -44,7 +44,7 @@ export const clientReviews = [
     position: 'Project Manager at UrbanTech ',
     img: 'assets/review3.png',
     review:
-      'I can’t say enough good things about Mitarth. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
+      "I can't say enough good things about Mitarth. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
   },
   {
     id: 4,
@@ -99,7 +99,7 @@ export const myProjects = [
     desc: 'LiveDoc is a powerful collaborative app that elevates the capabilities of real-time document editing. As an enhanced version of Google Docs, It supports millions of collaborators simultaneously, ensuring that every change is captured instantly and accurately.',
     subdesc:
       'With LiveDoc, users can experience the future of collaboration, where multiple contributors work together in real time without any lag, by using Next.js and Liveblocks newest features.',
-    href: 'https://www.youtube.com/watch?v=y5vE8y_f_OM',
+    href: 'https://liveditor-gilt.vercel.app/',
     texture: '/textures/project/project2.mp4',
     logo: '/assets/project-logo2.png',
     logoStyle: {
@@ -260,29 +260,29 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
 export const workExperiences = [
   {
     id: 1,
-    name: 'Framer',
-    pos: 'Lead Web Developer',
-    duration: '2022 - Present',
-    title: "Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
-    icon: '/assets/framer.svg',
+    name: 'Yen Ypga & Retreats',
+    pos: 'Web Developer',
+    duration: '2024',
+    title: 'Developed a modern, responsive website for a yoga retreat center, featuring class schedules, retreat booking, instructor profiles, and a blog to engage the wellness community.',
+    icon: '',
     animation: 'victory',
   },
   {
     id: 2,
-    name: 'Figma',
+    name: 'Audexis Consultancy',
     pos: 'Web Developer',
-    duration: '2020 - 2022',
-    title: "Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.",
-    icon: '/assets/figma.svg',
+    duration: '2024',
+    title: 'Built a consultancy platform specializing in transcription services, enabling clients to upload audio files, track project progress, and securely receive transcribed documents.',
+    icon: '',
     animation: 'clapping',
   },
   {
     id: 3,
-    name: 'Notion',
-    pos: 'Junior Web Developer',
-    duration: '2019 - 2020',
-    title: "Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
-    icon: '/assets/notion.svg',
+    name: 'FilamentFreaks',
+    pos: 'Web Developer',
+    duration: '2025',
+    title: 'Created an ecommerce website for 3D printers and tech accessories, with product listings, secure checkout, customer reviews, and inventory management for a seamless shopping experience.',
+    icon: '',
     animation: 'salute',
   },
 ];
